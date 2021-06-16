9c9145a85ef4132a39d9da558d13a41c6381d4ef
Amir Mussa <amir.mussa99@gmail.com>
2021-06-16 18:41:49 -0500

    browserRouter setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { LeftPanel } from './components';
 import './App.scss';
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <LeftPanel />
+        <Switch>
+          <Route path="/" exact>Home</Route>
+          <Route path="/explore">Explore</Route>
+          <Route path="/notifications">Notifications</Route>
+          <Route path="/messages">Messages</Route>
+          <Route path="/bookmarks">Bookmarks</Route>
+          <Route path="/lists">Lists</Route>
+          <Route path="/profile">Profile</Route>
+        </Switch>
+      </div>
+      <div>Right Panel</div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/components/leftPanel/leftPanel.jsx
@@ -0,0 +1,13 @@
+import './leftPanel.scss'
+
+const LeftPanel = () => {
+    return (
+        <div className="left-panel">
+            <div className="container">
+                <header>Twitter logo</header>
+            </div>
+        </div>
+    )
+}
+
+export default LeftPanel
